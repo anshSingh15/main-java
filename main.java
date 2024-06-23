9ad54f0f181f54cb58f5bf332e200e67ee6698a1
anshSingh15 <ansh23004@gmail.com>
2024-06-23 14:22:21 +0530

    first

---
+++ main.java
@@ -0,0 +1,10 @@
+package com.apnacollege;
+
+public class Main {
+    public static void main(String[] args) {
+
+        system.out.println("hello hey");
+        system.out.print("hello hey");
+    }
+}
+
